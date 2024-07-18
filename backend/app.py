83aa8d63096a42ffda5953b5a45537a792c4764a
smallpookie <frankn14@nycstudents.net>
2024-07-18 05:43:13 -0400

    Fixed code Errors such as missing remove_task

--- backend/app.py
+++ backend/app.py
@@ -9,7 +9,7 @@ class TaskManager:
     def add_task(self, task):
         self.tasks.append(task)
 
-    def add_task(self, task):
+    def remove_task(self, task):
         self.tasks.remove(task)
         
     def get_tasks(self):
@@ -21,5 +21,27 @@ manager = TaskManager()
 def get_tasks():
     tasks = manager.get_tasks()
     return jsonify(tasks)
+
+@app.route('/tasks', methods=['POST'])
+def add_task():
+    data = request.json
+    task = data.get('task')
+    if task:
+        manager.add_task(task)
+        return 'Task added', 201
+    else:
+        return 'Invalid request', 400
+    
+@app.route('/tasks/<task>', methods=['DELETE'])
+def remove_task(task):
+    try:
+        manager.remove_task(task)
+        return 'Task removed', 200
+    except ValueError:
+        return 'task not found', 400
+    
+if __name__=='__main__':
+    app.run(debug=True)
+            
     
            
